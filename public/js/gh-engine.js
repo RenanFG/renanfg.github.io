8cdd44e9c083e9e9fdd00d3bcbb2ca931d40c971
RenanFG <55338512+RenanFG@users.noreply.github.com>
2021-05-29 02:19:52 -0300

    Add files via upload

---
+++ public/js/gh-engine.js
@@ -0,0 +1,46 @@
+
+// GH engine simple template 
+
+// by : RenanFG @https://github.com/RenanFG
+
+// Licence: fodase
+
+
+// ---------------------------------------------------------------------------------
+
+window.onload = function(){
+  includeHTML();
+}
+
+
+
+function includeHTML() {
+  
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("system_include");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("system_include");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+
